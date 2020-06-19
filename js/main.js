a8cb642e738043e3626a1b59d0d58f93cf8ae843
hsuhau <hsuhau@foxmail.com>
2020-06-19 15:48:22 +0800

    ES6 新特性

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import util from "./hello.js";
+import {name, age, add} from "./user.js";
+
+util.sum(1,2);
+console.log(name);
+add(1,3);
---
+++ js/user.js
@@ -0,0 +1,10 @@
+var name = "jack"
+var age = 23;
+function add(a, b){
+    return a + b;
+}
+export {
+    name,
+    age,
+    add
+};
